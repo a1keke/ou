2c47a0338ac0f95d587d19d6ef74615cde7ddd19
a1keke <569215376@qq.com>
2017-11-25 19:11:17 +0800

    1

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+    let path = require('path');
+    let webpack = require('webpack');
+    module.exports = {
+        entry: {
+            // jianshu:path.resolve(__dirname,'src/jianshu/app.js'),
+            // biquge:path.resolve(__dirname,'src/biquge/app.js'),
+            // bookDetail:path.resolve(__dirname,'src/biquge/bookDetail.js'),
+            // readPage:path.resolve(__dirname,'src/biquge/readPage.js')
+            diary:path.resolve(__dirname,'src/diary/index.js')
+        },
+        output: {
+            path: path.resolve(__dirname,'static'),
+            filename: '[name].js',
+            publicPath: '/static/'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(png|jpg|jpeg|gif)$/,
+                    use: ['url-loader?limit=8192&name=/resource/[hash].[ext]'],
+                },
+                {
+                    test: /\.(mp4|ogg|svg)$/,
+                    use: ['file-loader?name=[hash].[ext]']
+                },
+                {
+                    test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: ['url-loader?limit=10000&=application/font-woff&name=[hash].[ext]'],
+                },
+                {
+                    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                    use: ['url-loader?limit=10000&=application/octet-stream&name=[hash].[ext]']
+                },
+                {
+                    test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                    use: ['file-loader?name=[hash].[ext]\'']
+                },
+                {
+                    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                    use: ['url-loader?limit=10000&=image/svg+xml&name=[hash].[ext]']
+                },
+                {
+                    test:/(\.jsx|\.js)$/,
+                    use:{loader: 'babel-loader'}
+                },
+                {
+                    test:/\.css$/,
+                    use:[
+                        {loader: "style-loader"},
+                        {loader: "css-loader", options: {modules:true}}
+                    ]
+                },
+                {
+                    test:/\.scss$/,
+                    use:[
+                        {loader: "style-loader"},
+                        {loader: "css-loader", options: {modules:true}},
+                        {loader: "sass-loader"}
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                React: 'react',
+                ReactDOM: 'react-dom',
+                PT: 'prop-types'
+            }),
+    /* 		new webpack.DllReferencePlugin({
+              context: __dirname,
+              manifest: require('./static/vendors-manifest.json')
+            })， */
+            // new webpack.optimize.UglifyJsPlugin({
+            //   compress: {
+            // 	warnings: false
+            //   }
+            // })
+        ],
+        resolve: {
+            modules: [
+                'node_modules',
+                path.resolve(__dirname, '/src'),
+                path.resolve(__dirname, '/dist'),
+                path.resolve(__dirname, '/src/common'),
+                path.resolve(__dirname, '/src/components'),
+                path.resolve(__dirname, '/src/layout'),
+                path.resolve(__dirname, '/src/view'),
+                path.resolve(__dirname, '/'),
+                path.resolve(__dirname, '/semantic'),
+            ]
+        },
+        watch: true
+    }
