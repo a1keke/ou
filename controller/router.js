ffb4cb728368eee242f4c973d6e1f3ccbe9a8132
a1keke <569215376@qq.com>
2017-12-14 12:30:59 +0800

    1

--- controller/router.js
+++ controller/router.js
@@ -116,3 +116,10 @@ exports.getDiary = function (req,res) {
         res.json(result)
     })
 }
+
+function getClientIp(req) {  
+        return req.headers['x-forwarded-for'] ||  
+        req.connection.remoteAddress ||  
+        req.socket.remoteAddress ||  
+        req.connection.socket.remoteAddress;  
+}  
