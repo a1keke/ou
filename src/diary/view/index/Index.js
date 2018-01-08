239a1002d934cf2a57b579b8204b474c4cd6db27
a1keke <569215376@qq.com>
2018-01-08 18:43:06 +0800

    scrollv0.1

--- src/diary/view/index/Index.js
+++ src/diary/view/index/Index.js
@@ -3,25 +3,42 @@ import DiaryTextarea from '../../components/diaryTextarea/DiaryTextarea.js';
 import DiaryList from '../../components/diaryList/DiaryList.js';
 import {connect} from 'react-redux';
 import {fetchDiaryList,fetchDeleteDiary} from '../../redux/action/index.js';
+import iScroll from 'iscroll/build/iscroll-probe.js';
 
 class index extends Component{
     constructor(props){
         super(props);
-
+        this.scrollEvent = this.scrollEvent.bind(this);
+        this.scrollEndEvent = this.scrollEndEvent.bind(this);
+        this.scrollBox = null;
     }
     componentDidMount(){
-        this.props.diaryList.length?'':this.props.fetchDiaryList();
+        if(!this.props.diaryList.length){
+            this.props.fetchDiaryList()
+        }
+        window.onscroll = ()=>{
+            console.log(1);
+        }
     }
     shouldComponentUpdate(nextProps,nextState){
         return JSON.stringify(nextProps.diaryList)!==JSON.stringify(this.props.diaryList)
-
+    }
+    scrollEvent(){
+        console.log(1);
+        return true;
+    }
+    scrollEndEvent(){
+        console.log(2);
+        return true;
     }
     render(){
         let {diaryList,account} = this.props;
         return (
-          <div>
-              {account?<DiaryTextarea/>:null}
-              <DiaryList {...{diaryList}}/>
+          <div id="diaryList" style={{height: window.innerHeight,position:'relative'}}>
+              <div style={{position:'absolute',width:'100%'}}>
+                  {account?<DiaryTextarea/>:null}
+                  <DiaryList {...{diaryList}}/>
+              </div>
           </div>
         );
     }
