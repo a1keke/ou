62b5d0fc6f86a481d552a9f92abd00c5f31e6e76
qianke <569215376@qq.com>
2018-02-26 11:49:31 +0800

    给首页增加了背景

--- src/root/app-root.js
+++ src/root/app-root.js
@@ -1,21 +1,22 @@
 import React,{Component} from 'react';
 import ReactDOM from "react-dom";
+import S from './style.scss';
 class Root extends Component{
     constructor(props){
         super(props);
     }
     render(){
         return (
-            <div className='pusher'>
-                <div className="ui inverted vertical masthead center aligned segment">
+            <div className={`pusher ${document.body.clientWidth>750?S.pcIndex:S.webIndex}`} style={{height:`${document.body.clientHeight}px`}}>
+                <div className="ui inverted vertical masthead center aligned segment" style={{position:'absolute',width:'100%'}}>
                     <div className="ui container">
                         <div className="ui large secondary inverted pointing menu">
                             <a className="item active">Home</a>
                             <a className="item" href='/diary'>Diary</a>
                             <a className="item" href='/biquge'>Book</a>
+                            <a className="item" href='/kele'>Kele</a>
                         </div>
                     </div>
-
                 </div>
             </div>
         );
